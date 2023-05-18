79f09827b5ca1425908bad4065fd4a233bac46e6
stevemoore9595 <stevemoore9595@gmail.com>
2023-05-18 16:25:21 -0600

    added readme info

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Social-Network-API
+
+## Description
+
+The Social Network app allows users to connect on social media. There is the ability to see all users, create a user, thought, and add a friend. They can also be updated and deleted.
+
+  ## Table of Contents
+
+  N/A
+
+  ## Installation
+
+  Run npm i to install inquirer.js, Mongoose package
+
+  ## License
+
+  MIT
+
+  ## Contributing
+
+  N/A
+
+  ## Deployment Link
+  
+  Click the following link to access the Social Network API walk through: https://watch.screencastify.com/v/p0CYYqAWgHL4ly9Bc8BS
+
+  ## Questions
+
+  - Click the following link to access my GitHub profile: https://github.com/stevemoore9595
+  
+  - For any additional questions, you may contact me at: stevemoore9595@gmail.com
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
+// const router = require('express').Router();
+// const userRoutes = require('./userRoutes');
+// const thoughtRoutes = require('./thoughtRoutes');
+
+
+// router.use('/users', userRoutes);
+// router.use('/thoughts', thoughtRoutes);
+
+// router.use((req, res) => res.send('Wrong route!'));
+
+// module.exports = router;
+
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+// const userRoutes = require('./userRoutes');
+// const thoughtRoutes = require('./thoughtRoutes');
+const apiRoutes = require('./api')
 
-
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+router.use('/api', apiRoutes);
+// router.use('/thoughts', thoughtRoutes);
 
 // router.use((req, res) => res.send('Wrong route!'));
 
